80ad40e4f3a38a7471042a2b8f3dcaf37582de61
ioanadanielailie <ioanadaniela71200@gmail.com>
2024-01-22 18:49:26 +0200

    Header

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("header.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${3000}`);
+});
